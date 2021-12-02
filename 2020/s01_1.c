3be83966aa7d090a03ff9a812edbf35ad6ff7034
Mihai Gaitos (ceres) <hawk@hawk.ro>
2021-12-02 19:18:33 +0200

    wip 2020 - 01

--- 2020/s01_1.c
+++ 2020/s01_1.c
@@ -7,64 +7,32 @@ int icmp(const void *v0, const void *v1)
 	return *a-*b;
 }
 
-/* search in array of length l, starting at position p, for pair that added yields s */
-int srch_pair(int *arr, int l, int p, int s)
-{
-	int i,x;
-	if(p>=l) return -1;
-	s-=arr[p];
-	for(i=l-1;i>p;i--)
-	{
-		x=arr[i];
-		if(x<s) return 0;
-		if(x==s) return i;
-	}
-	return 0;
-}
-
-int srch(int *arr, int l, int s, int *idx1, int *idx2)
-{
-	int i,x;
-	for(i=0;i<l-1;i++)
-	{
-		if((x=srch_pair(arr,l,i,s))!=0)
-		{
-			*idx1=i;
-			*idx2=x;
-			return 0;
-		}
-	}
-	return 1;
-}
-
-int srch(int *res, int *arr, int i0, int l, int lvl, int s)
+/* res = result array, arr = source array
+ * i0 = starting position, len = (source) array length
+ * lvl = level (depth) of search (i.e. when we need a triplet start at lvl 2
+ * s = sum that we search for */
+int srch(int *res, int *arr, int i0, int len, int lvl, int s)
 {
 	int i,x,rx;
-	if(i0 >= (l-1))
-		return -1;
-	s-=arr[i0];
-	for(i=i0+1;i<l;i++)
+	for(i=i0;i<len-lvl;i++)
 	{
 		x=arr[i];
 		if(x>s) return 1;
-		if(lvl==0)
+		if(lvl!=0) /* all but last level recurse one level down */
 		{
-			if(x==s)
+			rx=srch(res,arr,i+1,lvl-1,s-x);
+			if(rx==0)	/* success */
 			{
 				res[lvl]=i;
 				return 0;
 			}
-		}
-		else
-		{
-			rx=srch(res,arr,i,l,lvl-1,s);
-			if(rx==0)
-			{
-				res[lvl]=i;
-				return 0;
-			}
-			else if(rx<0)
+			else if(rx<0)	/* error, shouldn't happen */
 				return -1;
+		}
+		else if(x==s)	/* last level */
+		{
+			res[0]=i;
+			return 0;
 		}
 	}
 	return -1;
@@ -75,11 +43,12 @@ int main(int argc, char *argv[])
 	char *buf;
 	char **lines;
 	int *arr;
-	int lc,i;
+	int *res;
+	int lc,i,lvl,ds;
 	int idx1=0,idx2=0;
-	if(argc!=2)
+	if(argc!=4)
 	{
-		fprintf(stderr,"Usage: %s <filename>\n",argv[0]);
+		fprintf(stderr,"Usage: %s <filename> <values_cnt> <desired_sum>\n",argv[0]);
 		exit(1);
 	}
 	buf=file2buf(argv[1]);
@@ -94,8 +63,18 @@ int main(int argc, char *argv[])
 		fprintf(stderr,"Unable to read or no lines found\n");
 		exit(1);
 	}
+
+	lvl=atoi(argv[2]);
+	ds=atoi(argv[3]);
+	if(lvl<2 || ds<2)
+	{
+		fprintf(stderr,"values_cnt and desired_sum must be above 2\n");
+		exit(1);
+	}
+	
+	res=calloc(lvl,sizeof(int));
 	arr=calloc(lc,sizeof(int));
-	if(arr==NULL)
+	if(arr==NULL || res==NULL)
 	{
 		fprintf(stderr,"alloc error\n");
 		exit(1);
@@ -107,10 +86,18 @@ int main(int argc, char *argv[])
 	}
 	
 	qsort(arr,lc,sizeof(int),icmp);
-	if(srch(arr,lc,2020,&idx1,&idx2)==0)
-		printf("Found values %d and %d\n",arr[idx1],arr[idx2]);
+	if(srch(res,arr,0,lc,lvl-1,ds)==0)
+	{
+		printf("Found values. Printing...\n");
+		for(i=0;i<lvl;i++)
+			printf("Result %d = %d\n",i,res[i]);
+	}
 	else
 		printf("Not found\n");
-
+	
+	free(res);
+	free(arr);
+	free(lines);
+	free(buf);
 	return 0;
 }
